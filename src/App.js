289edc789ea90aaca74f7f6e1ac596c2477a14b0
IH-Tahim <imamhasan.01998@gmail.com>
2021-10-24 01:58:50 +0600

    footer component done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Col, Container, Form, InputGroup, Row, Button } from 'react-bootstrap';
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faPhoneAlt, faEnvelope, faMapMarkerAlt, faLocationArrow } from '@fortawesome/free-solid-svg-icons';
+import logoLight from "../../../logoWhite.svg";
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-dark">
+                <Container>
+                    <Row className="pt-3">
+                        <Col md={3} className="border_right ps-3">
+                            <h3 className="mb-4">Quick Contact</h3>
+                            <ul className="footer-ul">
+                                <li className="mb-3"><FontAwesomeIcon icon={faPhoneAlt} /> &ensp;Phone:
+                                    <a href="tel:+8801671234567">01671234567</a></li>
+                                <li className="mb-3"> <FontAwesomeIcon icon={faEnvelope} /> &ensp;Email:
+                                    <a href="mailto:support@medito.com">support@medito.com</a></li>
+                                <li><FontAwesomeIcon icon={faMapMarkerAlt} /> &ensp;Location:
+                                    12/3/jha mainka chipa road,Dhanmondi,Dhaka-1209</li>
+                            </ul>
+
+                        </Col>
+
+
+                        <Col md={5} className="text-center border_right px-3">
+                            <div className="border_bottom">
+                                <img src={logoLight} height="50" alt="" />
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nesciunt in doloribus adipisci asperiores iste, aliquam culpa totam atque voluptatum deleniti. Sed nesciunt, quaerat nobis voluptatum magni quis cumque aliquid.</p>
+                            </div>
+                            <div className="mt-3 mb-3">
+                                <h4>Subscribe to Newslatter</h4>
+                                <InputGroup hasValidation>
+
+                                    <Form.Control type="text" required className="form_bg" />
+                                    <Button> <FontAwesomeIcon icon={faLocationArrow} /> </Button>
+                                </InputGroup>
+                            </div>
+
+                        </Col>
+
+
+                        <Col md={4}>
+                            <h3 className="text-center">Opening Hours</h3>
+                            <p className="open-hr-table"><span>Mon - Tues : </span> <span>6.00 am - 10.00 pm</span></p>
+                            <p className="open-hr-table"><span>Wednes - Thurs :</span> <span>8.00 am - 6.00 pm</span></p>
+                            <p className="open-hr-table"><span>Friday : </span> <span>3.00 pm - 8.00 pm</span></p>
+                            <p className="open-hr-table"><span>Sunday : </span> <span>6.00 am - 10.00 pm</span></p>
+                            <p className="open-hr-table"><span>Saturday: </span> <span>10.00 am - 2.00 pm</span></p>
+                        </Col>
+                    </Row>
+
+                </Container>
+                <div className="footer_bottom py-3">
+                    Copyright ©2021 made with ❤ by Tahim. All Rights Reserved.
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
