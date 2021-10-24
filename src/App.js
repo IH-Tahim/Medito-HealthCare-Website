8b41e221ecaf1da52975625405c9c0adcfd846e8
IH-Tahim <imamhasan.01998@gmail.com>
2021-10-25 03:36:59 +0600

    BANNER done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
+import Banner from './Pages/Home/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Banner></Banner>
       <Footer></Footer>
     </div>
   );
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './Banner.css';
+
 
 const Banner = () => {
     return (
-        <div>
-            <img src="https://images2.imgbox.com/52/b1/aTUPLzsY_o.jpg" alt="" />
+        <div className="banner-container" >
+            <Container>
+                <Row>
+                    <Col md={5} className="py-5 align_banner_text">
+
+                        <div>
+                            <h2 className="text-white">We Provide Total</h2>
+                            <h1>Health care Solution</h1>
+                            <br />
+                            <p>Everyday e bring hope to milions of children in the world's hardest places as a sinn of god's unconditional.</p>
+                            <button className="btn btn-primary btn-lg me-3">Learn More</button>
+                            <button className="btn btn-primary btn-lg">View services</button>
+                        </div>
+
+                    </Col>
+                    <Col md={7}>
+
+                    </Col>
+                </Row>
+            </Container>
+
         </div>
     );
 };
